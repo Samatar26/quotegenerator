3dd5b05058cea08949f46435a56376cc854e0f67
Samatar26 <Samatar_267@hotmail.com>
2016-12-04 18:36:28 +0000

    Tweet Button added

--- js/index.js
+++ js/index.js
@@ -1,14 +1,17 @@
+
 function changeQuote(){$(document).ready(function(){
 $.getJSON('http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?', function (response){
+  
+  tweet= response.quoteAuthor;
+  
   if (response.quoteAuthor === ""){
     author.textContent="Unknown"
   }
 else{
   author.textContent=response.quoteAuthor;
 }  
-quoteText.innerText = response.quoteText;
-
-
+quoteText.textContent = response.quoteText;
+  twitter.href="https://twitter.com/intent/tweet?text="+response.quoteText;
 });
 });
 };
